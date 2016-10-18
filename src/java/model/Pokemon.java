3683ce7ff54a21a7296457fd739eeb8c727cc7fa
Amber Gerard <amber-j-gerard@uiowa.edu>
2016-10-17 19:06:49 -0500

    fixed codes in model

--- src/java/model/Pokemon.java
+++ src/java/model/Pokemon.java
@@ -8,4 +8,69 @@ public class Pokemon {
     private String Type;
     private String Location;
     private int StrengthRating;
+    
+    public Pokemon() {
+        this.PokemonID = 0;
+        this.PokemonName = "";
+        this.Type = "";
+        this.Location = "";
+        this.StrengthRating = 0;
+    }
+
+    public Pokemon(int PokemonID, String PokemonName, String Type, String Location, int StrengthRating) {
+        this.PokemonID = PokemonID;
+        this.PokemonName = PokemonName;
+        this.Type = Type;
+        this.Location = Location;
+        this.StrengthRating = StrengthRating;
+    }
+
+    public int getPokemonID() {
+        return PokemonID;
+    }
+
+    public void setPokemonID(int PokemonID) {
+        this.PokemonID = PokemonID;
+    }
+
+    public String getPokemonName() {
+        return PokemonName;
+    }
+
+    public void setPokemonName(String PokemonName) {
+        this.PokemonName = PokemonName;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String Type) {
+        this.Type = Type;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String Location) {
+        this.Location = Location;
+    }
+
+    public int getStrengthRating() {
+        return StrengthRating;
+    }
+
+    public void setStrengthRating(int StrengthRating) {
+        this.StrengthRating = StrengthRating;
+    }
+
+    @Override
+    public String toString() {
+        return "Pokemon{" + "PokemonID=" + PokemonID + ", PokemonName=" + PokemonName + ", Type=" + Type + ", Location=" + Location + ", StrengthRating=" + StrengthRating + '}';
+    }
+    
+    
+    
+    
 }
